a8624dc322f24eb62d21a32d1fcbdd0c05454a94
Bogdan Belenis <39201592+BogdanBelenis@users.noreply.github.com>
2023-10-02 16:16:37 +0200

    Update installationStart.sh

--- RaspberryPi4Ubuntu/installationStart.sh
+++ RaspberryPi4Ubuntu/installationStart.sh
@@ -3,7 +3,7 @@
 echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '">> ~/.bashrc
 
 sudo apt update -y
-sudo apt upgrade -y
+sudo apt full-upgrade -y 
 
 
 
